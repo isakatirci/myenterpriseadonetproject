6a23fbcd22fad3079d1115cfff86dabe25890834
isakatirci <ce.isakatirci@gmail.com>
2019-05-07 12:03:03 +0300

    Add project files.

---
+++ ProjectNameNorthwind.Mvc/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectNameNorthwind.Mvc.MvcApplication" Language="C#" %>
---
+++ ProjectNameNorthwind.Mvc/Models/Class.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectNameNorthwind.Mvc.Models
+{
+    public class Class
+    {
+        
+    }
+    
+}
